726b22b01482511adb105cf93a21d667ea1c87e5
koder-03 <163158547+koder-03@users.noreply.github.com>
2025-07-03 23:45:47 +0530

    Delete dice_challenge_files directory

--- dice_challenge_files/index.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  var left = Math.floor(Math.random() * 6) + 1;
-  var right = Math.floor(Math.random() * 6) + 1;
-   document.querySelector(".img1").setAttribute("src", "images/" + left + ".png");
-   document.querySelector(".img2").setAttribute("src", "images/" + right + ".png");
-
-};
-
