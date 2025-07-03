1d88f95eec5c9d1ff4661716226e9d0ceb0bf4a6
koder-03 <163158547+koder-03@users.noreply.github.com>
2025-07-03 23:56:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ window.onload = function () {
   var right = Math.floor(Math.random() * 6) + 1;
    document.querySelector(".img1").setAttribute("src", "images/" + left + ".png");
    document.querySelector(".img2").setAttribute("src", "images/" + right + ".png");
-
+   if (left > right) {
+    document.querySelector(".result").innerHTML = "player 1 won!! ";
+  } else if (right > left) {
+    document.querySelector(".result").innerHTML = "player 2 won!! ";
+  } else {
+    document.querySelector(".result").innerHTML = "DRAW!!! ";
+  }
 };
 
