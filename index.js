242689042e01f28f66ac54cfd3d1ae073f26c79c
koder-03 <163158547+koder-03@users.noreply.github.com>
2025-07-04 00:57:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 window.onload = function () {
-  var player1 = prompt("enter player 1 name");
-  var player2 = prompt("enter player 2 name");
-  document.querySelector(".left").innerHTML=player1;
-  document.querySelector(".right").innerHTML=player2;
-  document.querySelector(".play").onclick= function(){
-    var left = Math.floor(Math.random() * 6) + 1;
-  var right = Math.floor(Math.random() * 6) + 1;
-  document
-    .querySelector(".img1")
-    .setAttribute("src", "images/" + left + ".png");
-  document
-    .querySelector(".img2")
-    .setAttribute("src", "images/" + right + ".png");
-  if (left > right) {
-    document.querySelector(".result").innerHTML =  player1+" won!! ";
-  } else if (right > left) {
-    document.querySelector(".result").innerHTML = player2+" won!! ";
-  } else {
-    document.querySelector(".result").innerHTML = "DRAW!!! ";
-  }
-  }
+  var player1 = prompt("Enter Player 1 name:");
+  var player2 = prompt("Enter Player 2 name:");
 
-}
+  document.querySelector(".left").textContent = player1;
+  document.querySelector(".right").textContent = player2;
+
+  document.querySelector(".play").onclick = function () {
+    var left = Math.floor(Math.random() * 6) + 1;
+    var right = Math.floor(Math.random() * 6) + 1;
+
+    document.querySelector(".img1").setAttribute("src", "images/" + left + ".png");
+    document.querySelector(".img2").setAttribute("src", "images/" + right + ".png");
+
+    if (left > right) {
+      document.querySelector(".result").textContent = player1 + " won!! 🎉";
+    } else if (right > left) {
+      document.querySelector(".result").textContent = player2 + " won!! 🎉";
+    } else {
+      document.querySelector(".result").textContent = "DRAW!!! 🤝";
+    }
+  };
+};
+
 document.querySelector(".songbutton").addEventListener("click", function () {
   var audio = document.getElementById("myAudio");
-  if(audio.paused){
-
-      audio.play();
-  }
-  else{
+  if (audio.paused) {
+    audio.play();
+    this.textContent = "Pause the song ⏸️";
+  } else {
     audio.pause();
+    this.textContent = "Play our song 🎵";
   }
 });
