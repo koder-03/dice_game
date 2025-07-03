6e021dcb3cc7faa4fdd3a8edccdcddefdfc2a0e8
koder-03 <163158547+koder-03@users.noreply.github.com>
2025-07-04 00:17:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 window.onload = function () {
-  var player1= prompt("enter player 1 name");
-  var player2= prompt("enter player 2 name");
-  var left = Math.floor(Math.random() * 6) + 1;
+  var player1 = prompt("enter player 1 name");
+  var player2 = prompt("enter player 2 name");
+  document.querySelector(".left").innerHTML=player1;
+  document.querySelector(".right").innerHTML=player2;
+  document.querySelector(".play").onclick= function(){
+    var left = Math.floor(Math.random() * 6) + 1;
   var right = Math.floor(Math.random() * 6) + 1;
-   document.querySelector(".img1").setAttribute("src", "images/" + left + ".png");
-   document.querySelector(".img2").setAttribute("src", "images/" + right + ".png");
-   if (left > right) {
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "images/" + left + ".png");
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "images/" + right + ".png");
+  if (left > right) {
     document.querySelector(".result").innerHTML =  player1+" won!! ";
   } else if (right > left) {
     document.querySelector(".result").innerHTML = player2+" won!! ";
   } else {
     document.querySelector(".result").innerHTML = "DRAW!!! ";
   }
-};
+  }
 
+}
