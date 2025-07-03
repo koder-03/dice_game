d0bfc27bfb7561e31cf300e5b7f3d43f259f2ce9
koder-03 <163158547+koder-03@users.noreply.github.com>
2025-07-03 23:59:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 window.onload = function () {
+  var player1= prompt("enter player 1 name");
+  var player2= prompt("enter player 2 name");
   var left = Math.floor(Math.random() * 6) + 1;
   var right = Math.floor(Math.random() * 6) + 1;
    document.querySelector(".img1").setAttribute("src", "images/" + left + ".png");
    document.querySelector(".img2").setAttribute("src", "images/" + right + ".png");
    if (left > right) {
-    document.querySelector(".result").innerHTML = "player 1 won!! ";
+    document.querySelector(".result").innerHTML =  player1+" won!! ";
   } else if (right > left) {
-    document.querySelector(".result").innerHTML = "player 2 won!! ";
+    document.querySelector(".result").innerHTML = player2+" won!! ";
   } else {
     document.querySelector(".result").innerHTML = "DRAW!!! ";
   }
